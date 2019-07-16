84c627ff23527e448ecefe6ff44d3abdf3a4e2da
Sharath ST <sharathgowda899@gmail.com>
2019-07-16 20:10:51 +0530

    Add files via upload

---
+++ resume.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+cat<< "EOF"
+
+____    __    ____  _______  __        ______   ______   .___  ___.  _______     __   __   __
+\   \  /  \  /   / |   ____||  |      /      | /  __  \  |   \/   | |   ____|   |  | |  | |  |
+ \   \/    \/   /  |  |__   |  |     |  ,----'|  |  |  | |  \  /  | |  |__      |  | |  | |  |
+  \            /   |   __|  |  |     |  |     |  |  |  | |  |\/|  | |   __|     |  | |  | |  |
+   \    /\    /    |  |____ |  `----.|  `----.|  `--'  | |  |  |  | |  |____    |__| |__| |__|
+    \__/  \__/     |_______||_______| \______| \______/  |__|  |__| |_______|   (__) (__) (__)
+
+
+
+EOF
+
+cat << "EOF"
+
+
+   _____ _    _          _____         _______ _    _ _          _____  ______  _____ _    _ __  __ ______
+  / ____| |  | |   /\   |  __ \     /\|__   __| |  | ( )        |  __ \|  ____|/ ____| |  | |  \/  |  ____|
+ | (___ | |__| |  /  \  | |__) |   /  \  | |  | |__| |/ ___     | |__) | |__  | (___ | |  | | \  / | |__
+  \___ \|  __  | / /\ \ |  _  /   / /\ \ | |  |  __  | / __|    |  _  /|  __|  \___ \| |  | | |\/| |  __|
+  ____) | |  | |/ ____ \| | \ \  / ____ \| |  | |  | | \__ \    | | \ \| |____ ____) | |__| | |  | | |____
+ |_____/|_|  |_/_/    \_\_|  \_\/_/    \_\_|  |_|  |_| |___/    |_|  \_\______|_____/ \____/|_|  |_|______|
+
+EOF
+echo
+cat<<"EOF"
++-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+
+|C|O|N|T|A|C|T| |D|E|T|A|I|L|S|
++-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+
+EOF
+
+echo "
+Phone  : 7760809079
+E-mail : sharathgowda899@gmail.com
+Github : github.com/sharathbmsce
+"
+
+cat<<"EOF"
++-+-+-+-+-+
+|A|B|O|U|T|
++-+-+-+-+-+
+EOF
+
+echo "
+I am an Avid Learner and a Tech Enthusiast. A team worker with the zeal to learn new things and concepts quickly"
+echo
+echo
+cat<<"EOF"
++-+-+-+-+-+-+-+-+-+
+|E|D|U|C|A|T|I|O|N|
++-+-+-+-+-+-+-+-+-+
+EOF
+echo "
+   ____________________________________________________
+  | B.E (CSE)     | BMS College of Engineering |       |
+  |2016-present   |                            | 8.58  |
+  |_______________|____________________________|_______|
+  | PUC           | SRI Vidya Mandir           |       |
+  |2016           | PU College                 | 95.5% |
+  |_______________|____________________________|_______|
+  | SSLC          |  SRI Vidya Mandir          |       |
+  |2014           |                            | 92.3% |
+  |_______________|____________________________|_______|
+
+"
+echo
+cat <<"EOF"
++-+-+-+-+-+-+-+-+-+-+-+-+
+|A|C|H|I|E|V|E|M|E|N|T|S|
++-+-+-+-+-+-+-+-+-+-+-+-+
+EOF
+echo "
+  *Participated in the 24 hour Hackathon conducted at BMS College of Engineering to develop a Blockchain Application
+  *Completed all 3 levels of Vedic Mathematics in 2014
+  *Organised and conducted workshops on Basics of Arduino
+  *Won award for Best Idea in IOT at  the HPE day conducted by HPE at BMSCE
+"
+echo
+cat<<"EOF"
++-+-+-+-+-+-+-+-+
+|P|R|O|J|E|C|T|S|
++-+-+-+-+-+-+-+-+
+EOF
+echo "
+  *Smart Garbage Collector : An automated garbage collector arm built using Arduino board, servo motors ,RFID. Programmed using Arduino.
+  *Electricity Board Management System : Automated Bill collection and payment system . Developed using Oracle database and Visual Studio.
+  *Fraud Detection : A Blockchain application to check frauds of certificates and marks cards. Developed using Hyperledger Sawtooth and Dockers
+  *Student Forum : A web application for students to have discussions and queries. Developed using Django web development Framework
+  "
+echo
+cat<<"EOF"
++-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+|T|E|C|H|N|I|C|A|L| |S|K|I|L|L|S|
++-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+
+EOF
+echo "
+    *Programming Languages: C,C++,Java,Python
+    *Operating Systems : Linux, Windows
+    *Web Development : JavaScript, ReactJS ,HTML,CSS,Bootstrap, MongoDB, Jekyll
+    *Frameworks : Android Studio, Django ,Eclipse, React, Arduino
+    *Scripting Languages : Bash scripting
+    *Knowledge on DataStructures , Networking and Internet of Things.
+    *Knowledge on Blockchain  and containers like Docker
+    *Hands on experience with  Ethical Hacking and the tools used
+    *Mobile Application Development
+"
+echo
+cat<<"EOF"
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|C|E|R|T|I|F|I|C|A|T|I|O|N|S|
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+EOF
+echo "
+  *Certified Ethical Hacking (CEH v10)
+  *Intel’s Hyperledger Sawtooth
+"
+echo
+cat<<"EOF"
++-+-+-+-+-+-+-+-+-+
+|I|N|T|E|R|E|S|T|S|
++-+-+-+-+-+-+-+-+-+
+EOF
+echo "
+  *Basketball
+  *Linux
+  *Processors
+  *Android , Mobile Rooting
+  *Operating Systems
+"
+echo
+cat<<"EOF"
++-+-+-+-+-+-+-+-+-+
+|L|A|N|G|U|A|G|E|S|
++-+-+-+-+-+-+-+-+-+
+EOF
+echo "
+_________________Write___Read__Speak_
+| Kannada       |  ✔   |   ✔  |   ✔  |
+|_______________|______|______|______|
+| English       |  ✔   |   ✔  |   ✔  |
+|_______________|______|______|______|
+| Hindi         |      |   ✔  |   ✔  |
+|_______________|______|______|______|
+| Telugu        |      |   ✔  |   ✔  |
+|_______________|______|______|______|
+"
+echo
